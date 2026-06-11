60aeac7b16ea29fef1730924f94c991d56dd7451
Susmitha510 <susmi1005@gmail.com>
2026-06-11 09:05:05 +0530

    Prevent duplicate user registration

--- model/User.js
+++ model/User.js
@@ -1,12 +1,23 @@
-// models/User.js
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    resetToken: String,
-    resetTokenExpiry: Date
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    resetToken: {
+        type: String
+    },
+    resetTokenExpiry: {
+        type: Date
+    }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
